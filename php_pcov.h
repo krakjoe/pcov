9e8c1538972b354e8e6c97c057bf84e7a1407fb4
Remi Collet <remi@remirepo.net>
2021-03-22 07:00:19 +0100

    Prepare 1.0.8

--- php_pcov.h
+++ php_pcov.h
@@ -23,7 +23,7 @@
 extern zend_module_entry pcov_module_entry;
 #define phpext_pcov_ptr &pcov_module_entry
 
-#define PHP_PCOV_VERSION "1.0.8-dev"
+#define PHP_PCOV_VERSION "1.0.8"
 
 #ifdef PHP_WIN32
 #	define PHP_PCOV_API __declspec(dllexport)
